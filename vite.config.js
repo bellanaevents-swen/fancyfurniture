3bcc54d95dc56c74674a574757e0cce8abf42b3c
Swen <bellanaevents@gmail.com>
2026-08-04 17:56:33 +0300

    Clean up vite.config.js merge markers

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,5 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-<<<<<<< HEAD
-  base: '/fancyfurniture/', // Ensures relative path resolution when hosted on GitHub Pages
-=======
-  base: './', // Ensures relative path resolution when hosted on GitHub Pages
->>>>>>> ebbc304b4c0735547fd74bef7ac8eeb59b6114f1
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    emptyOutDir: true,
-  },
-});
+  base: '/fancyfurniture/',
+})

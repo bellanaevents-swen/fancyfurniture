5bc29262a03ac9f5e98bb617f31678d9aed1a125
Swen <bellanaevents@gmail.com>
2026-08-05 11:58:10 +0300

    Move images to public directory and fix asset paths

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,6 @@
+JavaScript
 import { defineConfig } from 'vite';
-import fs from 'fs';
-import path from 'path';
-
-function copyImagesPlugin() {
-  return {
-    name: 'copy-images',
-    closeBundle() {
-      const srcDir = path.resolve(__dirname, 'images');
-      const destDir = path.resolve(__dirname, 'dist/images');
-      if (fs.existsSync(srcDir)) {
-        fs.mkdirSync(destDir, { recursive: true });
-        fs.readdirSync(srcDir).forEach(file => {
-          fs.copyFileSync(path.join(srcDir, file), path.join(destDir, file));
-        });
-      }
-    }
-  };
-}
 
 export default defineConfig({
-  base: './',
-  plugins: [copyImagesPlugin()]
+  base: '/fancyfurniture/',
 });
